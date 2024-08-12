f9f6fa25a33e82cc71e4157e31cc9faed12663d3
anclaev <iahugo@yandex.ru>
2024-08-13 00:13:23 +0300

    feat(app): Структура проекта

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+'use client'
+
+export default function Error() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hi!
+      Error
     </main>
   )
 }
