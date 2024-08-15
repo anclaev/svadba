104a173b646b4216dd1ec089ab121e56c45f0f00
anclaev <iahugo@yandex.ru>
2024-08-15 17:38:52 +0300

    feat(auth): Подготовка авторизации

--- prisma/migrations/20240815134808_init/migration.sql
+++ prisma/migrations/20240815134808_init/migration.sql
@@ -14,31 +14,23 @@ CREATE TYPE "Side" AS ENUM ('GROOM', 'BRIDE');
 CREATE TYPE "Type" AS ENUM ('COLLEAGUE', 'FRIEND', 'BEST_FRIEND', 'RELATIVE', 'CLOSE_RELATIVE', 'PARENT');
 
 -- CreateTable
-CREATE TABLE "Account" (
-    "id" TEXT NOT NULL,
-    "status" "Status" NOT NULL DEFAULT 'PENDING',
-    "password" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Family" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
+    "ownerId" UUID NOT NULL,
     "name" TEXT NOT NULL,
-    "ownerId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATE NOT NULL,
 
     CONSTRAINT "Family_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Guest" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
+    "familyId" UUID,
+    "status" "Status" NOT NULL DEFAULT 'PENDING',
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT,
     "side" "Side" NOT NULL,
@@ -48,40 +40,41 @@ CREATE TABLE "Guest" (
     "table" INTEGER,
     "transfer" BOOLEAN NOT NULL DEFAULT false,
     "accommodation" BOOLEAN NOT NULL DEFAULT false,
-    "familyId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATE NOT NULL,
 
     CONSTRAINT "Guest_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Event" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "index" INTEGER NOT NULL,
     "alias" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "time" TIMESTAMP(3) NOT NULL,
+    "date" DATE NOT NULL,
+    "start" TIME(2) NOT NULL,
+    "end" TIME(2) NOT NULL,
     "description" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "address" TEXT,
+    "url" TEXT,
+    "createdAt" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATE NOT NULL,
 
     CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_EventToGuest" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Account_email_key" ON "Account"("email");
+CREATE UNIQUE INDEX "Family_ownerId_key" ON "Family"("ownerId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Family_ownerId_key" ON "Family"("ownerId");
+CREATE UNIQUE INDEX "Guest_email_key" ON "Guest"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Event_index_key" ON "Event"("index");
@@ -96,10 +89,10 @@ CREATE UNIQUE INDEX "_EventToGuest_AB_unique" ON "_EventToGuest"("A", "B");
 CREATE INDEX "_EventToGuest_B_index" ON "_EventToGuest"("B");
 
 -- AddForeignKey
-ALTER TABLE "Family" ADD CONSTRAINT "Family_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Family" ADD CONSTRAINT "Family_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Guest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Guest" ADD CONSTRAINT "Guest_familyId_fkey" FOREIGN KEY ("familyId") REFERENCES "Family"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Guest" ADD CONSTRAINT "Guest_familyId_fkey" FOREIGN KEY ("familyId") REFERENCES "Family"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_EventToGuest" ADD CONSTRAINT "_EventToGuest_A_fkey" FOREIGN KEY ("A") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
